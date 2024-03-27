c9d540ad88c3d9b61ecf480bb0369035acc56499
Gameefan <46563496+Gameefan@users.noreply.github.com>
2024-03-27 13:31:53 +0100

    [Addition] - PlayerBonuses

---
+++ Assembly-CSharp/ModLoader/PlayerBonuses.cs
@@ -0,0 +1,29 @@
+﻿
+namespace RL2.ModLoader;
+
+public class PlayerBonuses
+{
+    public int armorBonus;
+
+    public float critDamageBonus;
+
+    public float dexterityBonus;
+
+    public float magicCritDamageBonus;
+
+    public float focusBonus;
+
+    public float resolveBonus;
+
+    public int vitalityBonus;
+
+    public float strengthBonus;
+
+    public float inteligenceBonus;
+
+    public float healthBonus;
+
+    public int manaBonus;
+
+    public float movementSpeedBonus;
+}
