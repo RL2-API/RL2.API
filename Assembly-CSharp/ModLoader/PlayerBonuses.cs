05f5324f22f99b3e476e61813b5626992f48d158
Gameefan <46563496+Gameefan@users.noreply.github.com>
2024-03-27 15:50:50 +0100

    [Fix] - Initialize the PlayerBonuses object
    Otherwise the game won't load

--- Assembly-CSharp/ModLoader/PlayerBonuses.cs
+++ Assembly-CSharp/ModLoader/PlayerBonuses.cs
@@ -3,27 +3,27 @@ namespace RL2.ModLoader;
 
 public class PlayerBonuses
 {
-    public int armorBonus;
+    public int armorBonus = 0;
 
-    public float critDamageBonus;
+    public float critDamageBonus = 0.0f;
 
-    public float dexterityBonus;
+    public float dexterityBonus = 0.0f;
 
-    public float magicCritDamageBonus;
+    public float magicCritDamageBonus = 0.0f;
 
-    public float focusBonus;
+    public float focusBonus = 0.0f;
 
-    public float resolveBonus;
+    public float resolveBonus = 0.0f;
 
-    public int vitalityBonus;
+    public int vitalityBonus = 0;
 
-    public float strengthBonus;
+    public float strengthBonus = 0.0f;
 
-    public float inteligenceBonus;
+    public float inteligenceBonus = 0.0f;
 
-    public float healthBonus;
+    public float healthBonus = 0.0f;
 
-    public int manaBonus;
+    public int manaBonus = 0;
 
-    public float movementSpeedBonus;
+    public float movementSpeedBonus = 0.0f;
 }
