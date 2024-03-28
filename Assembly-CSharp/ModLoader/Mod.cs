a9a29e8162feac5ba7454b079e0a15b8da39de1a
Gameefan <46563496+Gameefan@users.noreply.github.com>
2024-03-28 11:30:35 +0100

    [Update] - Rename certain variables... ... and add new ones, generate a new patch with these changes in mind..

--- Assembly-CSharp/ModLoader/Mod.cs
+++ Assembly-CSharp/ModLoader/Mod.cs
@@ -5,8 +5,6 @@ namespace RL2.ModLoader;
 
 public abstract class Mod
 {
-    public abstract string Name { get; }
-
     public virtual void Load() { }
     
     public static void Log(string message)
