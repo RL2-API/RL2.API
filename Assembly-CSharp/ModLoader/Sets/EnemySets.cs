0051fbfd1575a0b5a87a890e303a72c1f980f74c
TacoConKvass <e_frun@o2.pl>
2024-04-08 10:57:31 +0200

    [Addition] - Fully functional GlobalEnemy

--- Assembly-CSharp/ModLoader/Sets/EnemySets.cs
+++ Assembly-CSharp/ModLoader/Sets/EnemySets.cs
@@ -2,7 +2,65 @@
 
 public class EnemySets
 {
-	public static readonly EnemyType[] AllEnemies = new EnemyType[] { EnemyType.Any };
+	public static readonly EnemyType[] AllEnemies = new EnemyType[]
+	{
+		EnemyType.Skeleton,
+		EnemyType.Wolf,
+		EnemyType.FlyingSword,
+		EnemyType.FlyingShield,
+		EnemyType.FlyingAxe,
+		EnemyType.FlyingHammer,
+		EnemyType.FlyingHunter,
+		EnemyType.Eggplant,
+		EnemyType.Zombie,
+		EnemyType.Eyeball,
+		EnemyType.SpearKnight,
+		EnemyType.SwordKnight,
+		EnemyType.BouncySpike,
+		EnemyType.Starburst,
+		EnemyType.FlyingBurst,
+		EnemyType.Slug,
+		EnemyType.RocketBox,
+		EnemyType.Ninja,
+		EnemyType.Flamer,
+		EnemyType.ArcThrower,
+		EnemyType.MimicChest,
+		EnemyType.Volcano,
+		EnemyType.Target,
+		EnemyType.Ghost,
+		EnemyType.Sniper,
+		EnemyType.RogueKnight,
+		EnemyType.BlobFish,
+		EnemyType.FlyingSkull,
+		EnemyType.JumpWallStick,
+		EnemyType.SkeletonArcher,
+		EnemyType.Plant,
+		EnemyType.AxeKnight,
+		EnemyType.StealthAssassin,
+		EnemyType.TopShotHazard,
+		EnemyType.Fireball,
+		EnemyType.ElementalFire,
+		EnemyType.ElementalIce,
+		EnemyType.ElementalBounce,
+		EnemyType.ElementalDash,
+		EnemyType.ElementalCurse,
+		EnemyType.SpellswordBoss,
+		EnemyType.Wisp,
+		EnemyType.Dummy,
+		EnemyType.DancingBoss,
+		EnemyType.SkeletonBossA,
+		EnemyType.SkeletonBossB,
+		EnemyType.StudyBoss,
+		EnemyType.MimicChestBoss,
+		EnemyType.PaintingEnemy,
+		EnemyType.EyeballBoss_Left,
+		EnemyType.EyeballBoss_Right,
+		EnemyType.EyeballBoss_Bottom,
+		EnemyType.EyeballBoss_Middle,
+		EnemyType.CaveBoss,
+		EnemyType.TraitorBoss,
+		EnemyType.FinalBoss
+	};
 
 	public static readonly EnemyType[] Bosses = new EnemyType[]
 	{
@@ -40,5 +98,5 @@ public class EnemySets
 		EnemyType.Wisp
 	};
 
-	public static readonly EnemyRank[] AllRanks = new EnemyRank[] { EnemyRank.Any };
+	public static readonly EnemyRank[] AllRanks = new EnemyRank[] { EnemyRank.Basic, EnemyRank.Advanced, EnemyRank.Expert, EnemyRank.Miniboss };
 }
---
+++ TestMod/TestGE.cs
@@ -0,0 +1,13 @@
+﻿using RL2.ModLoader;
+
+namespace TestMod;
+
+public class TestGE : GlobalEnemy
+{
+	public override EnemyRank[] AppliesToEnemyRank => base.AppliesToEnemyRank;
+
+	public void Update()
+	{
+		TestMod.Log("Spiuerldalal");
+	}
+}
