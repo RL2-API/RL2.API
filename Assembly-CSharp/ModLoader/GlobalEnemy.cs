e5493470f154154bc72aae2e2361c2d475fad523
TacoConKvass <e_frun@o2.pl>
2024-04-08 14:19:58 +0200

    [Patches] - Add GlobalEnemy patch

--- Assembly-CSharp/ModLoader/GlobalEnemy.cs
+++ Assembly-CSharp/ModLoader/GlobalEnemy.cs
@@ -6,9 +6,8 @@ namespace RL2.ModLoader;
 public abstract class GlobalEnemy : MonoBehaviour
 {
 	public EnemyController EnemyController => gameObject.GetComponent<EnemyController>();
-
 	public BaseAIScript AIScript => gameObject.GetComponent<BaseAIScript>();
-
 	public virtual EnemyType[] AppliesToEnemyType => EnemySets.AllEnemies;
 	public virtual EnemyRank[] AppliesToEnemyRank => EnemySets.AllRanks;
+	public virtual bool ActiveInRedPortals => true;
 }
