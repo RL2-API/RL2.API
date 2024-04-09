11ffb335b4ae3368eb0754261493cb98df407c73
TacoConKvass <e_frun@o2.pl>
2024-04-09 15:59:34 +0200

    [TestMod] - Update

---
+++ TestMod/BossStaggerGE.cs
@@ -0,0 +1,33 @@
+﻿using RL2.ModLoader;
+using RL2.ModLoader.Sets;
+using Steamworks;
+using System;
+using UnityEngine;
+
+namespace TestMod;
+
+public class BossStaggerGE : GlobalEnemy
+{
+	public override EnemyType[] AppliesToEnemyType => EnemySets.Bosses;
+
+	const int StaggerRequirement = 5;
+	public int CurrentHitCount = 0;
+	public int StaggerHitCooldown = 0;
+
+	public void Awake()
+	{
+		Messenger<GameMessenger, GameEvent>.AddListener(GameEvent.EnemyHit, OnHitBoss);
+	}
+	public void OnDestroy() { }
+
+	public void OnHitBoss(MonoBehaviour sender, EventArgs eventArgs)
+	{ 
+		if ((eventArgs as CharacterHitEventArgs).Victim != Enemy)
+		{
+			return;
+		}
+
+		CurrentHitCount++;
+		StaggerHitCooldown = 120;
+	}
+}
--- TestMod/TestGE.cs
+++ TestMod/TestGE.cs
@@ -1,9 +1,19 @@
 ﻿using RL2.ModLoader;
-using System.Linq;
 
 namespace TestMod;
 
 public class TestGE : GlobalEnemy
 {
 	public override EnemyType[] AppliesToEnemyType => new EnemyType[] { EnemyType.SpellswordBoss };
+
+	int Timer = 0;
+
+	public void Update()
+	{
+		Timer++;
+		if (Timer >= 600)
+		{
+			Enemy.SetLevel(10000);
+		}
+	}
 }
