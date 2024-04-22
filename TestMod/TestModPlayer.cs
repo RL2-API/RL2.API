24f7b19f2dda1ac4c8091b6ea0f96da313e78da1
TacoConKvass <e_frun@o2.pl>
2024-04-22 17:49:52 +0200

    [Update] - Stat bonuses

--- TestMod/TestModPlayer.cs
+++ TestMod/TestModPlayer.cs
@@ -4,8 +4,11 @@ namespace TestMod;
 
 public class TestModPlayer : ModPlayer
 {
-	public override void OnRelicChanged(RelicType relicType) {
-		Mod.Log(relicType.ToString());
+	public override void ModifyStats() {
+		PlayerController.StatBonuses.armor += 20;
+		PlayerController.StatBonuses.armorMultiplier += .25f;
+		PlayerController.StatBonuses.strength += 50;
+		PlayerController.CurrentArmor += 20;
 	}
 
 	public override void OnLoad(){
