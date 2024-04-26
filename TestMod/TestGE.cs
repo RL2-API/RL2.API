5c8a72ecc019300b20ac6716c16a7e063d6411d7
TacoConKvass <e_frun@o2.pl>
2024-04-26 09:11:52 +0200

    [Addition] - TestGE

---
+++ TestMod/TestGE.cs
@@ -0,0 +1,18 @@
+﻿using RL2.ID;
+using RL2.ModLoader;
+using System.Collections.Generic;
+
+namespace TestMod;
+
+public class TestGE : GlobalEnemy
+{
+	public override Dictionary<int, EnemyRank[]> AppliesToEnemy => new Dictionary<int, EnemyRank[]>() {
+		{EnemyID.Starburst, new EnemyRank[] { EnemyRank.Basic, EnemyRank.Advanced }},
+		{EnemyID.SwordKnight, new EnemyRank[] { EnemyRank.Basic, EnemyRank.Advanced, EnemyRank.Expert, EnemyRank.Miniboss } }
+	};
+
+	public override void OnSpawn()
+	{
+		Mod.Log("Works");
+	}
+}
