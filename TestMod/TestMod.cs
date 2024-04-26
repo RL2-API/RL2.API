e081543afaae8bf0432824bb39e3fcb8eb9b18a7
TacoConKvass <e_frun@o2.pl>
2024-04-26 09:12:28 +0200

    [Readdition] - set-gold, get-heirlooms commands

--- TestMod/TestMod.cs
+++ TestMod/TestMod.cs
@@ -8,4 +8,30 @@ public class TestMod : Mod
 	public override void OnLoad() { 
 		Log("CawaBunga");
 	}
+
+	[Command("set-gold")]
+	public static void SetGold(string[] args)
+	{
+		if (args.Length == 0)
+		{
+			Log("You need to specify the amount of gold you want");
+			return;
+		}
+		if (!int.TryParse(args[0], out int money))
+		{
+			Log("The amount of gold must be an integer");
+			return;
+		}
+		SaveManager.PlayerSaveData.GoldCollected = money;
+	}
+
+	[Command("get-heirlooms")]
+	public static void GetHeirlooms(string[] args)
+	{
+		foreach (HeirloomType type in typeof(HeirloomType).GetEnumValues())
+		{
+			if (type == HeirloomType.None) continue;
+			SaveManager.PlayerSaveData.SetHeirloomLevel(type, 1, false, true);
+		}
+	}
 }
