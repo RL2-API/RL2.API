9529f5c6534ec8e7a10520db6353709a8913e6b8
TacoConKvass <e_frun@o2.pl>
2024-04-26 21:10:13 +0200

    [Removal] - Unnecessary code from UnityHook

--- Assembly-CSharp/ModLoader/UnityHook.cs
+++ Assembly-CSharp/ModLoader/UnityHook.cs
@@ -5,25 +5,19 @@ namespace RL2.ModLoader;
 
 public class UnityHook : MonoBehaviour
 {
-	public static Console Console = new Console();
-
 	public void Awake() {
 		DontDestroyOnLoad(this);
 		gameObject.AddComponent<Console>();
-		if (ModLoader.LoadedMods != null) {
-			foreach (Mod mod in ModLoader.LoadedMods) {
-				foreach (Type modSystem in mod.ModSystems) {
-					ModSystem modSystemInstance = gameObject.AddComponent(modSystem) as ModSystem;
-					if (!modSystemInstance.IsLoadingEnabled()) {
-						Destroy(modSystemInstance);
-						continue;
-					}
-					modSystemInstance.OnLoad();
+		
+		foreach (Mod mod in ModLoader.LoadedMods) {
+			foreach (Type modSystem in mod.ModSystems) {
+				ModSystem modSystemInstance = gameObject.AddComponent(modSystem) as ModSystem;
+				if (!modSystemInstance.IsLoadingEnabled()) {
+					Destroy(modSystemInstance);
+					continue;
 				}
+				modSystemInstance.OnLoad();
 			}
-		}
-		else {
-			Debug.Log("ModLoader.LoadedMods is null");
 		}
 	}
 }
