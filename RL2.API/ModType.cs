ef07b52e0582d83c3be180c7139ac2ee34bfc130
TacoConKvass <e_frun@o2.pl>
2024-06-09 20:22:22 +0200

    [Addition] - ModType port

---
+++ RL2.API/ModType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace RL2.ModLoader;
+
+/// <summary>
+/// A parent class grouping all types provided by the RL2.API into one
+/// </summary>
+public abstract class ModType : MonoBehaviour { }
