e9f8101ddb921ee53d688e7cadd11a67032435db
TacoConKvass <e_frun@o2.pl>
2024-06-09 20:22:08 +0200

    [Addition] - Mod fields port and documentation

--- RL2.API/Mod.cs
+++ RL2.API/Mod.cs
@@ -1,12 +1,45 @@
+using System;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
 namespace RL2.ModLoader;
 
+/// <summary>
+/// A clas required for your mod to be recognised as using the RL2.API
+/// </summary>
 public abstract class Mod
 {
+	/// <summary>
+	/// Path to the mod, set at load
+	/// </summary>
+	public string Path { get; set; }
+
+	/// <summary>
+	/// All types from this mod.
+	/// </summary>
+	internal Type[] Content;
+
+	/// <summary>
+	/// Gets all types inheriting from T.
+	/// </summary>
+	/// <typeparam name="T">The type you want to get derived classes of.</typeparam>
+	public Type[] GetModTypes<T>() where T : ModType => Content.Where(type => type.IsSubclassOf(typeof(T))).ToArray();
+
+	/// <summary>
+	/// Ran right after loading all mods.
+	/// </summary>
 	public virtual void OnLoad() { }
+	
+	/// <summary>
+	/// 
+	/// </summary>
 	public virtual void OnUnload() { }
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="message"></param>
 	public static void Log(object message) {
 		Debug.Log($"[{Assembly.GetCallingAssembly().GetName().Name}]: {message}");
 	}
