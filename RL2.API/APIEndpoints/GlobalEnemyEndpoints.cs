6ac2e9cc3ea4599708de2a181c76f200fc0eef2a
TacoConKvass <e_frun@o2.pl>
2024-06-10 21:31:16 +0200

    [Change] - Renamed the endpoints directory to APIEndpoints

---
+++ RL2.API/APIEndpoints/GlobalEnemyEndpoints.cs
@@ -0,0 +1,82 @@
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+
+namespace RL2.ModLoader.APIEndpoints;
+
+/// <summary>
+/// Stores all RL2.API endpoints for the GlobalEnemy class
+/// </summary>
+public class GlobalEnemyEndpoints
+{
+	/// <summary>
+	/// Handles attaching GlobalEnemy objects to the enemies and running OnSpawn
+	/// </summary>
+	public static Hook AttachGlobalEnemyInstnces = new Hook(
+		typeof(EnemyController).GetMethod("Start", BindingFlags.NonPublic | BindingFlags.Instance),
+		AttachGlobalEnemyInstance
+	);
+
+	/// <summary>
+	/// <inheritdoc cref="AttachGlobalEnemyInstnces"/>
+	/// </summary>
+	public static IEnumerator AttachGlobalEnemyInstance(Func<EnemyController, IEnumerator> orig, EnemyController self) {
+		yield return orig(self);
+		foreach (Mod mod in APIStore.LoadedMods) {
+			foreach (Type globalEnemy in mod.GetModTypes<GlobalEnemy>()) {
+				GlobalEnemy globalEnemyInstance = (GlobalEnemy)self.gameObject.AddComponent(globalEnemy);
+
+				// If the AppliesToEnemy dictionary is empty, assume that this GlobalEnemy should be applied to every enemy.
+				if (globalEnemyInstance.AppliesToEnemy.Keys.Count == 0) {
+					globalEnemyInstance.OnSpawn();
+					continue;
+				}
+
+				if (globalEnemyInstance.AppliesToEnemy.TryGetValue((int)self.EnemyType, out EnemyRank[] enemyRanks)) {
+					if (enemyRanks.IndexOf(self.EnemyRank) != -1) {
+						globalEnemyInstance.OnSpawn();
+						continue;
+					}
+				}
+
+				UnityEngine.Object.Destroy(globalEnemyInstance);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Handles death prevention for enemies
+	/// </summary>
+	public static Hook PreKill = new Hook(
+		typeof(EnemyController).GetMethod("KillCharacter", BindingFlags.Public | BindingFlags.Instance),
+		(Action<EnemyController, GameObject, bool> orig, EnemyController self, GameObject killer, bool broadcastEvents) => {
+			bool PreventDeath = false;
+			foreach (GlobalEnemy globalEnemy in self.gameObject.GetComponents<GlobalEnemy>()) {
+				if (!globalEnemy.PreKill(killer)) {
+					PreventDeath = true;
+					break;
+				}
+			}
+			if (PreventDeath) {
+				self.SetHealth(1f, additive: false, runEvents: true);
+				return;
+			}
+			orig(self, killer, broadcastEvents);
+		}
+	);
+
+	/// <summary>
+	/// Handles calling <seealso cref="GlobalEnemy.OnKill"/>
+	/// </summary>
+	public static Hook OnKill = new Hook(
+		typeof(EnemyController).GetMethod("KillCharacter", BindingFlags.Public | BindingFlags.Instance),
+		(Action<EnemyController, GameObject, bool> orig, EnemyController self, GameObject killer, bool broadcastEvents) => {
+			orig(self, killer, broadcastEvents);
+			foreach (GlobalEnemy globalEnemy in self.gameObject.GetComponents<GlobalEnemy>()) {
+				globalEnemy.OnKill(killer);
+			}
+		}
+	);
+}
