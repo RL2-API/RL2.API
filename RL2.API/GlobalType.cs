bfd7ae55696973dae2063d81626250f7687591f2
TacoConKvass <e_frun@o2.pl>
2024-06-10 21:33:20 +0200

    [Addition] - GlobalType

---
+++ RL2.API/GlobalType.cs
@@ -0,0 +1,6 @@
+namespace RL2.ModLoader;
+
+/// <summary>
+/// A parent class grouping all global types provided by the RL2.API into one
+/// </summary>
+public abstract class GlobalType : ModType { }
