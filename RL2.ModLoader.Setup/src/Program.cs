e8295cfaca5adec74b7497a6c061e6916f665f58
TacoConKvass <e_frun@o2.pl>
2024-06-12 15:20:18 +0200

    [Update] - RL2.ModLoader.Setup now accepts an argument as an output path

--- RL2.ModLoader.Setup/src/Program.cs
+++ RL2.ModLoader.Setup/src/Program.cs
@@ -1,19 +1,31 @@
 using System;
 using System.IO;
-using System.Linq;
 
 public class Program
 {
-	static readonly string ConfigPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "game-install-path.txt";
-	static readonly string OutputLibPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase.Replace("RL2.ModLoader.Setup\\", "") + "source\\lib\\";
+	static readonly string ConfigPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "config\\game-install-path.txt";
+	static string OutputLibPath = AppDomain.CurrentDomain.SetupInformation.ApplicationBase.Replace("RL2.ModLoader.Setup\\", "") + "source\\lib\\";
 
 	public static void Main(string[] args) {
 		if (!File.Exists(ConfigPath)) {
+			if (!Directory.Exists(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "config")) {
+				Directory.CreateDirectory(AppDomain.CurrentDomain.SetupInformation.ApplicationBase + "config");
+			}
 			Console.WriteLine("Provide your Rogue Legacy 2 installation path");
 			string Path = Console.ReadLine();
 			File.WriteAllText(ConfigPath, Path + "\\");
 		}
 		
+		if (args != null) {
+			if (args.Length != 0) {
+				OutputLibPath = args[0] + "\\";
+			}
+		}
+
+		if (!Directory.Exists(OutputLibPath)) {
+			Directory.CreateDirectory(OutputLibPath);
+		}
+
 		Console.WriteLine("\nStart copying libs...");
 		string LibrariesLocation = File.ReadAllText(ConfigPath) + "Rogue Legacy 2_Data\\Managed\\";
 		DirectoryInfo librariesDirectory = new DirectoryInfo(LibrariesLocation);
@@ -21,5 +33,7 @@ public class Program
 			Console.WriteLine("Copying " + fileInfo.Name + " to " + OutputLibPath + fileInfo.Name);
 			File.Copy(fileInfo.FullName, OutputLibPath + fileInfo.Name, true);
 		}
+		Console.WriteLine("Press any key to close...");
+		Console.ReadKey();
 	}
 }
