94d2ea83b24d918182a3b51f2bae938a71d4b018
TacoConKvass <e_frun@o2.pl>
2024-07-03 06:46:58 +0200

    [Update] - Updated installer to keep up with the directory changes

--- RL2.ModLoader.Installer/Program.cs
+++ RL2.ModLoader.Installer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using NativeFileDialogSharp;
@@ -18,41 +19,64 @@ public class Program
 		DialogResult result = Dialog.FolderPicker();
 		string DataPath = result.Path + "\\Rogue Legacy 2_Data";
 		string ManagedPath = DataPath + "\\Managed";
+		string ModsPath = DataPath + "\\Mods";
 
 		if (!Directory.Exists(DataPath) || !Directory.Exists(ManagedPath)) {
 			Console.WriteLine("The provided path is incorrect");
 			return;
 		}
 
-		// Copy .json files
-		Copy("RuntimeInitializeOnLoads.json", DataPath);
-		Copy("ScriptingAssemblies.json", DataPath);
+		Console.WriteLine("Install RL2.API? Press y(es) or n(o)");
+		bool installRL2API = Console.ReadKey().KeyChar == 'y';
+		Console.WriteLine("");
 
-		// Copy RL2.API assemblies
+		// Copy RL2.ModLoader necessities
 		Copy("RL2.ModLoader.xml", ManagedPath);
 		Copy("RL2.ModLoader.dll", ManagedPath);
 		Copy("RL2.ModLoader.pdb", ManagedPath);
-		Copy("RL2.API.dll", ManagedPath);
-		Copy("RL2.API.xml", ManagedPath);
-		Copy("RL2.API.pdb", ManagedPath);
+		Copy("RuntimeInitializeOnLoads.json", DataPath);
+		Copy("ScriptingAssemblies.json", DataPath);
 
 		// Copy MonoMod files
-		Copy("Mono.Cecil.dll", ManagedPath);
-		Copy("Mono.Cecil.Rocks.dll", ManagedPath);
-		Copy("MonoMod.Common.dll", ManagedPath);
-		Copy("MonoMod.Common.xml", ManagedPath);
-		Copy("MonoMod.RuntimeDetour.dll", ManagedPath);
-		Copy("MonoMod.RuntimeDetour.xml", ManagedPath);
-		Copy("MonoMod.Utils.dll", ManagedPath);
-		Copy("MonoMod.Utils.xml", ManagedPath);
+		string[] MonoModFilenames = [
+			"Mono.Cecil.dll",
+			"Mono.Cecil.Rocks.dll",
+			"MonoMod.Common.dll",
+			"MonoMod.Common.xml",
+			"MonoMod.RuntimeDetour.dll",
+			"MonoMod.RuntimeDetour.xml",
+			"MonoMod.Utils.dll",
+			"MonoMod.Utils.xml"
+		];
 
+		foreach (string fileName in MonoModFilenames) {
+			File.Copy(CurrentPath + "\\MonoMod\\" + fileName, ManagedPath + "\\" + fileName, true);
+			Console.Write("|");
+			Thread.Sleep(10);
+		}
+
+		// Copy RL2.API
+		if (installRL2API) {
+			if (!Directory.Exists(ModsPath)) {
+				Directory.CreateDirectory(ModsPath);
+			}
+			if (!Directory.Exists(ModsPath + "\\RL2.API")) {
+				Directory.CreateDirectory(ModsPath + "\\RL2.API");
+			}
+			Copy("RL2.API\\RL2.API.dll", ModsPath);
+			Copy("RL2.API\\RL2.API.xml", ModsPath);
+			Copy("RL2.API\\RL2.API.pdb", ModsPath);
+			Copy("RL2.API\\RL2.API.mod.json", ModsPath);
+		}
+
+		// Finish up
 		Console.WriteLine("\n\nInstallation complete. Press any key to exit...");
 		Console.ReadKey();
 	}
 
 	private static void Copy(string sourceFile, string destPath)
 	{
-        File.Copy(CurrentPath + "//" + sourceFile, destPath + "//" +  sourceFile, true);
+        File.Copy(CurrentPath + "\\" + sourceFile, destPath + "\\" +  sourceFile, true);
         Console.Write("|");
 		Thread.Sleep(10);
     }
