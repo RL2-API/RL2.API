8c747bf8cecfe5775a8a5bdf243517c1564c6ab2
TacoConKvass <e_frun@o2.pl>
2025-01-22 10:17:28 +0100

    [Change] - Enemy.ModifyDamageTaken signature

--- RL2.API/APIEndpoints/Enemy.cs
+++ RL2.API/APIEndpoints/Enemy.cs
@@ -103,15 +103,19 @@ public static class Enemy
 	/// </summary>
 	/// <param name="enemyDamaged">The enemy damaged</param>
 	/// <param name="damageSource">Object that damaged the enemy</param>
-	/// <param name="damageTaken">How much damage the enemy received</param>
+	/// <param name="damageTaken">How much damage the enemy received. Do not multiply, use <paramref name="damageMultiplier"/></param>
+	/// <param name="damageMultiplier">Used for percentage based/multiplicative bonuses</param>
 	/// <param name="critType">Critical strike type</param>
-	/// <param name="trueDamage">Set to true if the damage should not be affected by modifiers</param>
-	public delegate void ModifyDamageTaken_delegate(EnemyController enemyDamaged, IDamageObj damageSource, ref float damageTaken, ref CriticalStrikeType critType, bool trueDamage);
+	public delegate void ModifyDamageTaken_delegate(EnemyController enemyDamaged, IDamageObj damageSource, ref float damageTaken, ref float damageMultiplier, ref CriticalStrikeType critType);
 
 	/// <inheritdoc cref="ModifyDamageTaken_delegate"/>
 	public static event ModifyDamageTaken_delegate? ModifyDamageTaken;
 
 	internal static void ModifyDamageTaken_Invoke(EnemyController enemyDamaged, IDamageObj damageSource, ref float damageTaken, ref CriticalStrikeType critType, bool trueDamage) {
-		ModifyDamageTaken?.Invoke(enemyDamaged, damageSource, ref damageTaken, ref critType, trueDamage);
+		if (!trueDamage) {
+			float damageMultiplier = 1f;
+			ModifyDamageTaken?.Invoke(enemyDamaged, damageSource, ref damageTaken, ref damageMultiplier, ref critType);
+			damageTaken *= damageMultiplier;
+		}
 	}
 }
