20281c6777930950e033f22fb61c4b1ec0fda97c
TacoConKvass <e_frun@o2.pl>
2025-05-20 15:15:00 +0200

    [Add] Consts for future commits

---
+++ RL2.API/Consts.cs
@@ -0,0 +1,8 @@
+namespace RL2.API;
+
+public partial class RL2API {
+	// I hate the fact those 2 are needed
+	internal const string AssemblyCsharp_FullName = "Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+	internal const string AssemblyCsharp_FullTypeNameSuffix = ", Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+}
