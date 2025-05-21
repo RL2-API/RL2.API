9821c977461e82113b7344c59f849bd8f39ef996
TacoConKvass <e_frun@o2.pl>
2025-05-21 16:08:58 +0200

    [Fix:API] Indentation

--- RL2.API/APIEndpoints/Map.cs
+++ RL2.API/APIEndpoints/Map.cs
@@ -8,34 +8,34 @@ namespace RL2.API;
 /// Provides endpoints for map related APIs
 /// </summary>
 public static class Map {
-		/// <summary>
-		/// Allows assigning a custom map icon to a room
-		/// </summary>
-		/// <param name="roomToCheck">Room to modify the map icon for</param>
-		/// <param name="getUsed">Is the room "finished" (failed/completed Fairy Chest, empty relic room etc.)</param>
-		/// <param name="isMergeRoom">Is the room made out of multiple rooms</param>
-		/// <returns>
-		/// The texture used as the map icon <br/>	
-		/// <see langword="null"/> to follow vanilla logic
-		/// </returns>
-		public delegate Texture2D? ModifyRoomIcon_delegate(GridPointManager roomToCheck, bool getUsed, bool isMergeRoom);
+	/// <summary>
+	/// Allows assigning a custom map icon to a room
+	/// </summary>
+	/// <param name="roomToCheck">Room to modify the map icon for</param>
+	/// <param name="getUsed">Is the room "finished" (failed/completed Fairy Chest, empty relic room etc.)</param>
+	/// <param name="isMergeRoom">Is the room made out of multiple rooms</param>
+	/// <returns>
+	/// The texture used as the map icon <br/>	
+	/// <see langword="null"/> to follow vanilla logic
+	/// </returns>
+	public delegate Texture2D? ModifyRoomIcon_delegate(GridPointManager roomToCheck, bool getUsed, bool isMergeRoom);
 
-		/// <inheritdoc cref="ModifyRoomIcon_delegate"/>
-		public static event ModifyRoomIcon_delegate? ModifyRoomIcon;
+	/// <inheritdoc cref="ModifyRoomIcon_delegate"/>
+	public static event ModifyRoomIcon_delegate? ModifyRoomIcon;
 
-		internal static Texture2D? ModifyRoomIcon_Invoke(GridPointManager roomToCheck, bool getUsed, bool isMergeRoom) {
-			if (ModifyRoomIcon is null) {
-				return null;
-			}
-
-			Texture2D? modifiedIcon = null;
-			foreach (Delegate subscriber in ModifyRoomIcon.GetInvocationList()) {
-				if (modifiedIcon != null) {
-					break;
-				}
-
-				modifiedIcon = (Texture2D?)subscriber.DynamicInvoke(roomToCheck, getUsed, isMergeRoom);
-			}
-			return modifiedIcon;
+	internal static Texture2D? ModifyRoomIcon_Invoke(GridPointManager roomToCheck, bool getUsed, bool isMergeRoom) {
+		if (ModifyRoomIcon is null) {
+			return null;
 		}
+
+		Texture2D? modifiedIcon = null;
+		foreach (Delegate subscriber in ModifyRoomIcon.GetInvocationList()) {
+			if (modifiedIcon != null) {
+				break;
+			}
+
+			modifiedIcon = (Texture2D?)subscriber.DynamicInvoke(roomToCheck, getUsed, isMergeRoom);
+		}
+		return modifiedIcon;
 	}
+}
