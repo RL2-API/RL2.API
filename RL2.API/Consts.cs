ea9bb1d7ec982847ba40bd2a02d1ef459a010e41
TacoConKvass <e_frun@o2.pl>
2025-05-22 17:16:35 +0200

    [Add:API] More consts :D

--- RL2.API/Consts.cs
+++ RL2.API/Consts.cs
@@ -5,4 +5,8 @@ public partial class RL2API {
 	internal const string AssemblyCsharp_FullName = "Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
 
 	internal const string AssemblyCsharp_FullTypeNameSuffix = ", Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+	internal const string RL2API_FullName = "RL2.API, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+	
+	internal const string RL2API_FullTypeNameSuffix = ", RL2.API, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
 }
