7b2e8982f959ed96641698065061c8611dc55cc2
TacoConKvass <e_frun@o2.pl>
2025-05-26 13:20:40 +0200

    [Update] Setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ rm -rf RL2.ModLoader.DevSetup
 
 echo "Downloading dev setup..."
 echo ""
-curl -OL https://github.com/RL2-API/RL2.ModLoader.DevSetup/releases/download/v2.0.0/RL2.ModLoader.DevSetup.tar.gz
+curl -OL https://github.com/RL2-API/RL2.ModLoader.DevSetup/releases/download/v2.2.0/RL2.ModLoader.DevSetup.tar.gz
 mkdir RL2.ModLoader.DevSetup
 tar -xzf ./"RL2.ModLoader.DevSetup.tar.gz" -C ./RL2.ModLoader.DevSetup
 rm ./"RL2.ModLoader.DevSetup.tar.gz"
