e3043fb7702a8891915063e7c91a82c915a97beb
TacoConKvass <e_frun@o2.pl>
2025-08-17 10:14:29 +0200

    [Fix:API] Docs

--- RL2.API/Endpoints/Relics.cs
+++ RL2.API/Endpoints/Relics.cs
@@ -89,7 +89,7 @@ public static class Relics
 	}
 
 	/// <summary>
-	/// Ran when Relic data is being loaded. <br></br>
+	/// Ran when Relic data is being saved. <br></br>
 	/// Current save profile canm be accessed via <see cref="SaveManager.CurrentProfile"/>
 	/// </summary>
 	public static class SaveData {
@@ -142,7 +142,8 @@ public static class Relics
 	}
 
 	/// <summary>
-	/// Ran when Relic data is being loaded.
+	/// Ran when Relic data is being loaded.<br></br>
+	/// Current save profile canm be accessed via <see cref="SaveManager.CurrentProfile"/>
 	/// </summary>
 	public static class LoadData
 	{
