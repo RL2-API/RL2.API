7a8f1d221f8c505c3c54756fd1833f29761d4142
TacoConKvass <e_frun@o2.pl>
2026-01-28 21:01:38 +0100

    Remove: Useless files TODO: Update README

--- package.sh
+++
@@ -1,9 +0,0 @@
-rm -rf package
-
-mkdir package
-cd bin/Release
-tar -a -cf ../../package/RL2.API.zip RL2.API
-echo Packaged RL2.API
-tar -a -cf ../../package/ExampleMod.zip ExampleMod
-echo Packaged ExampleMod
-cd ../..
